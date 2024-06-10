4f02927bdc999c381acf82a118240e900467cd2d
Mariano Maisonnave <mariano.maisonnave@cs.uns.edu.ar>
2024-06-10 18:27:19 -0300

    - Auxiliary function to compute relevant metrics (such as precision and accuracy) from yhat and ytrue vectors).

--- metrics.py
+++ metrics.py
@@ -1,6 +1,25 @@
 import pandas as pd
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix,accuracy_score
 
+
+def get_metric_evaluations_from_yhat_and_ypred(y_true, y_pred, y_score=None, description=None):
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+    results = {'Description': '' if description is None else description,
+                        'Accuracy': accuracy_score(y_true, y_pred),
+                        'Precision': precision_score(y_true, y_pred),
+                        'Recal': recall_score(y_true, y_pred),
+                        'F1-Score': f1_score(y_true, y_pred),
+                        'TN': tn,
+                        'TP': tp,
+                        'FN': fn,
+                        'FP': fp
+                        }
+    if not y_score is None:
+        results['AUC'] = roc_auc_score(y_true=y_true, y_score=y_score)
+
+    results = {key: [value] for key, value in results.items()}
+    return pd.DataFrame(results)
+
 def get_metric_evaluations(evaluated_model,
                             X_train,
                             y_true,
